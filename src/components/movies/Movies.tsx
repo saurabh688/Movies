a93f74be1a79348b8db6afb2c98ef75e896bdf7a
saurabh688 <saurabhrai34@gmail.com>
2023-07-13 18:18:05 -0400

    Update Movies.tsx

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,18 +1,25 @@
 import "./Movies.css";
 const Movies = (props: any) => {
+  console.log("propssss", props);
   return (
     <div className="Movies" key={props.data.id}>
       <div>
-        <h4 style={{ margin: "0px" }}>{props.data.title}</h4>
-        <hr />
+        <h4 style={{margin:"0px"}}>{props.data.title} &nbsp;&nbsp; {props.data.original_language === "en"?"":`  ${props.data?.original_title}`}</h4>
+        <hr/>
         <p>
           <b>Language:</b>
           {props.data.original_language}
+          <span style={{float:"right"}}>
+          &nbsp;&nbsp;&nbsp;&#9733;{props.data.vote_average} 
+          </span>
+          
         </p>
         <p>
           <b>Release Date:</b>
           {props.data.release_date}
+          
         </p>
+
       </div>
 
       <br />
@@ -26,9 +33,7 @@ const Movies = (props: any) => {
       <h5 className="movieSummary">{props.data.overview}</h5>
       <br />
       {props.isAdd ? (
-        <button className="MovieButton" onClick={props.onClick}>
-          Add to favorite
-        </button>
+        <button className="MovieButton" onClick={props.onClick}>Add to favorite</button>
       ) : null}
     </div>
   );
